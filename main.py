1b912609ad03684424915f908d6caa58c2d8f6b6
Shane Powell <srpowell812@gmail.com>
2018-10-12 21:27:07 -0500

    Added simple mechanics.

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 # Main project file
+
+class Character:
+    """Class for a character"""
+
+    def __init__(self, attack, hp):
+        self.attack
+        self.hp
+
+    def attacked(self, attack):
+        self.hp = self.hp - attack
+        if self.hp < 0:
+            self.hp = 0
+
+moe = Character(50,5)
+joe = Character(65,14)
+
+moe.attacked()
+
+
+
